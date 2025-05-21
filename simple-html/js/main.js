246502cb5d7cfbd3e373ad40a3615524e7095480
juniorrxcx <juniorrxcx13@icloud.com>
2025-05-21 19:34:43 -0300

    Aplicação de confirmação para link externo

--- simple-html/js/main.js
+++ simple-html/js/main.js
@@ -1,2 +1,14 @@
-let nome = "Junior";
-alert("Boa noite " + nome);
+const linkExterno = 
+        document.querySelector('a[title="Google Maps"]');
+
+//linkExterno.onclick = () => {
+  //  alert("Você será redirecionado para um link externo.");
+//};
+
+linkExterno.addEventListener('click', (e) => {
+        const option = 
+                confirm("Você será redirecionado para um link externo.");
+        if (option === false) {
+                e.preventDefault();
+        }
+});
